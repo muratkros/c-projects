c29190ef823f24b018bdf2150221f15b42f09cc0
muratkros <muratkros61@gmail.com>
2026-03-08 12:38:35 +0300

    ceyrekdaire alani

---
+++ ceyrekdaire.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+using namespace std;
+int main(int argc,char** argv){
+    float yaricap,alan;
+    cout<<"Lutfen ceyrek dairenin yaricapini giriniz: "<<endl;
+    cin>>yaricap;
+    alan=(3.14*pow(yaricap,2))/4;
+    cout<<"Ceyrek dairenin alani: "<<alan<<endl;
+    }
