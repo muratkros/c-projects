ed537a70e6ab04ed0016ea044fc8f18388514630
muratkros <muratkros61@gmail.com>
2026-04-17 19:48:52 +0300

    alpo dosyalari eklendi

--- alpo.cpp
+++ alpo.cpp
@@ -1,39 +1,14 @@
-#include <cstdlib>
 #include <iostream>
+#include <string>
+#include <cmath>
 using namespace std;
-int main(int argc, char *argv[])
-{
-    double sayi1, sayi2;
-    char sembol;
-
-    cout << "Yapmak istediginiz islemi secin (+, -, *, /): " << endl;
-    cin >> sembol;
-
-    cout << "birinci sayiyi giriniz: " << endl;
-    cin >> sayi1;
-    cout << "ikinci sayiyi giriniz: " << endl;
-    cin >> sayi2;
-
-    switch(sembol)
-    {
-        case '+':
-            cout << "Sonuc: " << sayi1 + sayi2 << endl;
-            break;
-        case '-':
-            cout << "Sonuc: " << sayi1 - sayi2 << endl;
-            break;
-        case '*':
-            cout << "Sonuc: " << sayi1 * sayi2 << endl;
-            break;
-        case '/':
-            if (sayi2 != 0)
-                cout << "Sonuc: " << sayi1 / sayi2 << endl;
-            else
-                cout << "Hata: Bir sayi sifira bolunemez!" << endl;
-            break;
-        default:
-            cout << "Gecersiz islem secimi!" << endl;
-    }
-
+int main(int argc, char*argv) {
+    double a, b;
+    string alperen;
+    cin >> alperen;
+    alperen.append("Furkan");
+    cout << alperen << endl;
+    cin >> a >> b;  
+    cout << abs(sqrt(pow(a + b,2))) << endl;
     return 0;
 }
